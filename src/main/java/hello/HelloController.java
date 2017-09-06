9055b622dfd1dfefb51facdbbd406aea307cca76
karthik78180 <karthik78180@gmail.com>
2017-09-06 17:11:00 -0500

    initial commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "deploy spring boot application to Spring PCF and display the custom logs";
+    }
+    
+}
